1c788d2343fb286ed8df0346d1c96f49dfc96d77
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2024-11-04 20:23:02 -0300

    fix: user service

--- src/modules/users/domain/ports/input/user.service.ts
+++ src/modules/users/domain/ports/input/user.service.ts
@@ -5,6 +5,7 @@ import { UserService } from './UserService';
 import { UserResponse } from 'src/modules/users/adapters/model/request/user-response.dto';
 import { UserRepositoryImpl } from 'src/modules/users/adapters/driven/user.repository.impl';
 import { UserRequest } from 'src/modules/users/adapters/model/response/user-request.dto';
+import { UserMapper } from '../../mapper/user.mapper';
 
 @Injectable()
 export class UserServiceImpl implements UserService {
@@ -14,23 +15,17 @@ export class UserServiceImpl implements UserService {
   ) {}
 
   async find(): Promise<UserResponse[]> {
-    // const users = await this.userRepository.findAll();
-    // return users.map(user => UserMapper.toResponse(user));
-
-    return null
+    const users = await this.userRepository.findAll();
+    return users.map(user => UserMapper.toResponse(user));
   }
 
   async findById(id: number): Promise<UserResponse> {
-    // const user = await this.userRepository.findById(id);
-    // return UserMapper.toResponse(user);
-
-    return null
+    const user = await this.userRepository.findById(id);
+    return UserMapper.toResponse(user);
   }
 
   async save(userRequest: UserRequest): Promise<UserResponse> {
-    // const user = UserMapper.toEntity(userRequest);
-    // return UserMapper.toResponse(await this.userRepository.save(user));
-
-    return null
+    const user = UserMapper.toEntity(userRequest);
+    return UserMapper.toResponse(await this.userRepository.save(user));
   }
 }
